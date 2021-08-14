2b9b697c0e09f305f6a1746828fca733346ea1ca
Poype <253186926@qq.com>
2021-08-14 12:18:34 +0800

    Project Init & SpringSecurity加载原理分析
    SpringSecurity是利用一些列的Filter实现的，涉及到的Filter如下
    1. DelegatingFilterProxy：它是SpringMvc中的类，它的作用是在Tomcat Container和Spring Container之间建立桥梁，以便在Tomcat Filter
                              中能够使用Spring Bean资源。
                              其实它就是一个proxy，在它内部会获取到Spring Bean，进而调用Spring Bean。但它要求调用的Spring Bean也必须
                              实现Filter接口。
    2. FilterChainProxy：它也实现了Filter接口，它会挂载到DelegatingFilterProxy上，它是SpringSecurity的总入口。
    3. SecurityFilterChain：对一次request的安全检查会用到 N 个Filter，这些所有的Filter构成的一个整体就是SecurityFilterChain。
                            FilterChainProxy通过SecurityFilterChain获取一系列的Filter，再循环依次调用每个Filter的doFilter方法
                            执行安全检查。
                            FilterChainProxy中可以包含多个SecurityFilterChain，通过SecurityFilterChain的matches方法判断选择
                            哪个SecurityFilterChain处理这次request。默认使用的实现类是 DefaultSecurityFilterChain。
    4. SecurityFilter：真正对一次request进行安全检查的地方，一个SecurityFilterChain包含任意数量的SecurityFilter。
                       SpringBoot默认配置的SecurityFilter如截图所示

---
+++ src/main/java/com/poype/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.poype.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "hello security";
+    }
+}
+
+/**
+ * DelegatingFilterProxy： SpringBoot自动配置
+ *       FilterChainProxy: 它是SpringSecurity的入口，从名字也能够看出它是一系列SpringSecurityFilter的代理。它下面可以有多个SecurityFilterChain
+ *       SecurityFilterChain：it is used by FilterChainProxy to determine which Spring Security Filters should be invoked for this request.
+ *          SecurityFilter：一个SecurityFilterChain中可以包含任意数量的SecurityFilter
+ */
+
+
+
+// org.springframework.security.web.DefaultSecurityFilterChain
+//
